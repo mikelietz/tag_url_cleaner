65c0f5b84057e0f973e2a06402a9b1c907908484
mikelietz <codger+git@gmail.com>
2012-05-29 18:29:52 -0400

    Initial commit.

---
+++ tag_url_cleaner.plugin.php
@@ -0,0 +1,16 @@
+<?php
+class TagURLCleaner extends Plugin
+{
+	public function filter_rewrite_rules( $rules )
+	{
+		foreach( $rules as $rule ) {
+			if( $rule->name == 'display_entries_by_tag' ) {
+				$rule->parse_regex = '#^(?P<tag>[^/]*)(?:/page/(?P<page>\d+))?/?$#i';
+				$rule->build_str = '{$tag}(/page/{$page})';
+				return $rules;
+			}
+		}
+		return $rules;
+	}
+}
+?>
